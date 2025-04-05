4c1921181b69316c69d8accd81f865c8de3ca1e6
Philip Holm Møller <phmo61881@edu.ucl.dk>
2025-04-05 13:06:39 +0200

    Add project files.

---
+++ AntiFoodWasteBlazor/Shared/Models/FoodWasteProduct.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AntiFoodWasteBlazor.Shared.Models
+{
+    public class FoodWasteProduct
+    {
+        public string Description { get; set; }
+        public decimal NewPrice { get; set; }
+        public decimal OriginalPrice { get; set; }
+        public string ImageUrl { get; set; }
+        public string StoreName { get; set; }
+        public string StoreAddress { get; set; }
+    }
+}
